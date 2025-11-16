40121758f64d7ffba9048cd26bce4ed059475e36
Wojciech Kulik <3128467+wojciech-kulik@users.noreply.github.com>
2025-11-16 17:21:59 +0100

    update xcp to 1.2.0

--- Formula/xcp.rb
+++ Formula/xcp.rb
@@ -1,8 +1,8 @@
 class Xcp < Formula
   desc "A fast, lightweight command-line tool for managing Xcode projects - built entirely in Swift."
   homepage "https://github.com/wojciech-kulik/XcodeProjectCLI"
-  url "https://github.com/wojciech-kulik/XcodeProjectCLI/releases/download/v1.1.0/xcp.zip"
-  sha256 "0b7a4cfbfa6720294c1f8caa269b9b490d5e0af82f147a80032aeac1f163cfd6"
+  url "https://github.com/wojciech-kulik/XcodeProjectCLI/releases/download/v1.2.0/xcp.zip"
+  sha256 "7157020918552a490e63089c4f33b0e47dd4752a567e1a142dac4334694df428"
   license "MIT"
 
   depends_on macos: :ventura
@@ -12,7 +12,7 @@ class Xcp < Formula
   end
 
   test do
-    assert_match "1.1.0", shell_output("#{bin}/xcp --version")
+    assert_match "1.2.0", shell_output("#{bin}/xcp --version")
   end
 end
 
